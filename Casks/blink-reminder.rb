b183e99fa09af613771ebe9e9784cc81720a1ef2
ntn0de <ntn0de@gmail.com>
2026-01-13 12:50:03 +0545

    chore: update sha256 for v1.0.3 release

--- Casks/blink-reminder.rb
+++ Casks/blink-reminder.rb
@@ -1,6 +1,6 @@
 cask "blink-reminder" do
   version "1.0.3"
-  sha256 ""
+  sha256 "b59b37e87368eeba1f25f73fd0261ac555f03aab355758c16c55d6c03aa89dfa"
 
   url "https://github.com/ntn0de/blink-reminder/releases/download/v#{version}/BlinkReminder.zip"
   name "BlinkReminder"
